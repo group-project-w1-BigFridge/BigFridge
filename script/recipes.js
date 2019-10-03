38b20bc8dca0ebf6675ca7303250e15b08a6e6cd
Satyo Wicaksana <satyowicaksana@gmail.com>
2019-10-04 05:51:00 +0700

    recipes page done

---
+++ script/recipes.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+    $('#ingredients-form').submit((e) => {
+        e.preventDefault()
+        $.ajax({
+            url: 'http://localhost:3000/recipes',
+            method: 'post',
+            data: {
+                ingredients: $('#ingredients').val()
+            }
+        })
+        .done(recipes => {
+            console.log(recipes)
+            $('#recipes-container').empty()
+            recipes.forEach(recipe => {
+                let usedIngredients = '<ul>'
+                recipe.usedIngredients.forEach(usedIngredient => {
+                    console.log(usedIngredient)
+                    usedIngredients += `<li style="color: #6ec96d; list-style-type: none;"><i class='fas fa-circle' style='font-size:10px'></i> ${usedIngredient.name}</li>`
+                })
+                usedIngredients += '</ul>'
+                let missedIngredients = '<ul>'
+                recipe.missedIngredients.forEach(missedIngredient => {
+                    console.log(missedIngredient)
+                    missedIngredients += `<li style="color: #c95555; list-style-type: none;"><i class='fas fa-circle' style='font-size:10px'></i> ${missedIngredient.name}</li>`
+                })
+                missedIngredients += '</ul>'
+                $('#recipes-container').append(`
+                <div class="media">
+                <img src="${recipe.image}" class="rounded-circle" style="width: 100px; height: 100px; padding-top: 5px;" alt="${recipe.title}">
+                <div class="media-body" style="margin-left: 20px;">
+                    <h5 class="mt-0">${recipe.title}</h5>
+                    <div class="row">
+                    <div class="column" style="width: 200px;">${usedIngredients}</div>
+                    <div class="column" style="width: 250px;">${missedIngredients}</div>
+                    </div>
+                </div>
+                </div>
+                <hr>
+                `)
+            });
+        })
+        .fail(err => {
+            console.log(err)
+        })
+    })
+});
