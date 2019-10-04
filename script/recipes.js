384883ca15803e8c7a1685233f7182a03c8ad562
Satyo Wicaksana <satyowicaksana@gmail.com>
2019-10-04 07:19:29 +0700

    error handling

--- script/recipes.js
+++ script/recipes.js
@@ -9,38 +9,45 @@ $(document).ready(function() {
             }
         })
         .done(recipes => {
-            console.log(recipes)
-            $('#recipes-container').empty()
-            recipes.forEach(recipe => {
-                let usedIngredients = '<ul>'
-                recipe.usedIngredients.forEach(usedIngredient => {
-                    console.log(usedIngredient)
-                    usedIngredients += `<li style="color: #6ec96d; list-style-type: none;"><i class='fas fa-circle' style='font-size:10px'></i> ${usedIngredient.name}</li>`
-                })
-                usedIngredients += '</ul>'
-                let missedIngredients = '<ul>'
-                recipe.missedIngredients.forEach(missedIngredient => {
-                    console.log(missedIngredient)
-                    missedIngredients += `<li style="color: #c95555; list-style-type: none;"><i class='fas fa-circle' style='font-size:10px'></i> ${missedIngredient.name}</li>`
-                })
-                missedIngredients += '</ul>'
-                $('#recipes-container').append(`
-                <div class="media">
-                <img src="${recipe.image}" class="rounded-circle" style="width: 100px; height: 100px; padding-top: 5px;" alt="${recipe.title}">
-                <div class="media-body" style="margin-left: 20px;">
-                    <h5 class="mt-0">${recipe.title}</h5>
-                    <div class="row">
-                    <div class="column" style="width: 200px;">${usedIngredients}</div>
-                    <div class="column" style="width: 250px;">${missedIngredients}</div>
+            if(recipes.length === 0) {
+                swal('No recipe found', 'try another keywords', "info");
+            } else {
+                $('#recipes-container').empty()
+                recipes.forEach(recipe => {
+                    let usedIngredients = '<ul>'
+                    recipe.usedIngredients.forEach(usedIngredient => {
+                        console.log(usedIngredient)
+                        usedIngredients += `<li style="color: #6ec96d; list-style-type: none;"><i class='fas fa-circle' style='font-size:10px'></i> ${usedIngredient.name}</li>`
+                    })
+                    usedIngredients += '</ul>'
+                    let missedIngredients = '<ul>'
+                    recipe.missedIngredients.forEach(missedIngredient => {
+                        console.log(missedIngredient)
+                        missedIngredients += `<li style="color: #c95555; list-style-type: none;"><i class='fas fa-circle' style='font-size:10px'></i> ${missedIngredient.name}</li>`
+                    })
+                    missedIngredients += '</ul>'
+                    $('#recipes-container').append(`
+                    <div class="media">
+                    <img src="${recipe.image}" class="rounded-circle" style="width: 100px; height: 100px; padding-top: 5px;" alt="${recipe.title}">
+                    <div class="media-body" style="margin-left: 20px;">
+                        <h5 class="mt-0">${recipe.title}</h5>
+                        <div class="row">
+                        <div class="column" style="width: 200px;">${usedIngredients}</div>
+                        <div class="column" style="width: 250px;">${missedIngredients}</div>
+                        </div>
                     </div>
-                </div>
-                </div>
-                <hr>
-                `)
-            });
+                    </div>
+                    <hr>
+                    `)
+                });
+            }
         })
         .fail(err => {
-            console.log(err)
+            if(!err.responseJSON) {
+                swal('Connection lost', 'Something wrong with the server', "error"); 
+            } else {
+                swal(err.responseJSON.message, '', "error");
+            }
         })
     })
 });
